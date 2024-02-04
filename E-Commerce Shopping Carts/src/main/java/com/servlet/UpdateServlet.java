16b323384b69ae62b3f88adc6d89b4ed780b8625
shubham <98804207+Shubham-sakunde@users.noreply.github.com>
2024-02-04 22:33:31 +0530

    Initial commit
    Uploading all project files to GitHub. This project is a simple note-taking web app using Java, JSP, Servlets, and Hibernate for backend logic, with Bootstrap for the frontend. This commit marks the initial version.

---
+++ E-Commerce Shopping Carts/src/main/java/com/servlet/UpdateServlet.java
@@ -0,0 +1,53 @@
+package com.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.entities.Note;
+import com.helper.FactoryProvider;
+
+
+public class UpdateServlet extends HttpServlet {
+	
+    public UpdateServlet() {
+        super();
+       
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		try {
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			int noteId = Integer.parseInt(request.getParameter("noteId"));
+			
+			Session s = FactoryProvider.getSessionFactory().openSession();
+			Transaction tx = s.beginTransaction();
+			
+			Note note = s.get(Note.class, noteId);
+			
+			note.setTitle(title);
+			note.setContent(content);
+			note.setAddedDate(new Date());
+		
+			tx.commit();
+			s.close();
+			
+			response.sendRedirect("all_notes.jsp");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+}
